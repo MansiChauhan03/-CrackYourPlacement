2fd601b1f03d84bc1cebcbc016d99e3e3f9d69d5
Mansi Chauhan <149080468+MansiChauhan03@users.noreply.github.com>
2024-07-24 22:20:01 +0530

    Create MaximumPointsYouCanObtainFromCards.cpp

---
+++ MaximumPointsYouCanObtainFromCards.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        int totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            totalSum += cardPoints[i];
+        }
+        
+        if (k == n) {
+            return totalSum;
+        }
+        int windowSum = 0;
+        for (int i = 0; i < n - k; i++) {
+            windowSum += cardPoints[i];
+        }
+        
+        int minWindowSum = windowSum;
+        for (int i = n - k; i < n; i++) {
+            windowSum += cardPoints[i] - cardPoints[i - (n - k)];
+            minWindowSum = min(minWindowSum, windowSum);
+        }
+        return totalSum - minWindowSum;
+    }
+};
