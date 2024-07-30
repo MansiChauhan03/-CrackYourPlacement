589a2b6917feae39778ac1ade4016045e7976672
Mansi Chauhan <149080468+MansiChauhan03@users.noreply.github.com>
2024-07-30 14:16:05 +0530

    Update NumberOfIslands.cpp

--- NumberOfIslands.cpp
+++ NumberOfIslands.cpp
@@ -1,3 +1,5 @@
+SOLUTION 1:
+ 
 class Solution {
  public:
   int numIslands(vector<vector<char>>& grid) {
@@ -26,4 +28,36 @@ class Solution {
     dfs(grid, i, j + 1);
     dfs(grid, i, j - 1);
   }
+};
+
+
+SOLUTION 2:
+ 
+class Solution {
+public:
+    void dfs(int row, int col, vector<vector<char>> &grid, vector<vector<int>> &vis){
+        if(row<0 || row>=grid.size() || col<0 || col>=grid[0].size() || vis[row][col] || grid[row][col] == '0') return;
+
+        vis[row][col] = 1;
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[]  = {0, -1, 0, 1};
+        for(int i = 0;  i<4; i++){
+            dfs(row+delRow[i], col+delCol[i], grid, vis);
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(!vis[i][j] && grid[i][j] == '1'){
+                    ans++;
+                    dfs(i, j, grid, vis);
+                }
+            }
+        }
+        return ans;
+    }
 };
