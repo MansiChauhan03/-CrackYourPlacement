187a43f96506758a49413cea8d0256850b9c6771
Mansi Chauhan <149080468+MansiChauhan03@users.noreply.github.com>
2024-07-30 14:21:11 +0530

    Update ExcelSheetColumnTitle.cpp

--- ExcelSheetColumnTitle.cpp
+++ ExcelSheetColumnTitle.cpp
@@ -1,3 +1,5 @@
+SOLUTION 1:
+    
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
@@ -9,5 +11,22 @@ public:
             columnNumber = (columnNumber-1)/26;
         }
         return str;
+    }
+};
+
+SOLUTION 2:
+
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string ans = "";
+        while(n>0){
+            n--;
+            int curr = n % 26;
+            n = n/26;
+            ans.push_back((char)(curr + 'A'));
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
