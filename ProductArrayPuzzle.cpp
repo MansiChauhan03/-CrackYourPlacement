d714e0fe3933685cd085a33ddee56c2e0c694745
Mansi Chauhan <149080468+MansiChauhan03@users.noreply.github.com>
2024-08-02 08:27:33 +0530

    Create ProductArrayPuzzle.cpp

---
+++ ProductArrayPuzzle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<long long int> productExceptSelf(vector<long long int>& nums) {
+        int n = nums.size();
+        vector<long long int> ans(n, 1);
+        long long int leftProduct = 1, rightProduct = 1;
+        
+        // Calculate left products
+        for (int i = 0; i < n; ++i) {
+            ans[i] *= leftProduct;
+            leftProduct *= nums[i];
+        }
+        
+        // Calculate right products
+        for (int i = n - 1; i >= 0; --i) {
+            ans[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+        
+        return ans;
+    }
+};
